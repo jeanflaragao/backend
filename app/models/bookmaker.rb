bbcb9c5ac5e197ff1f8af039b9198ee2caff9b7a
Jean Aragão <aragao.jean@gmail.com>
2026-05-11 20:20:43 -0300

    feat(bookmakers): add currency attribute to `Bookmaker` model and migration

--- app/models/bookmaker.rb
+++ app/models/bookmaker.rb
@@ -1,2 +1,3 @@
 class Bookmaker < ApplicationRecord
+  validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_05_11_225916) do
+ActiveRecord::Schema[8.0].define(version: 2026_05_11_230944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,5 +21,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_05_11_225916) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "currency"
   end
 end
