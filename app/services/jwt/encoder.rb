712b1f2465ec95dcd74ec73a20123fb02b7667bc
Jean Aragão <aragao.jean@gmail.com>
2026-05-13 16:13:21 -0300

    feat(auth): implement jwt `encoder` and `decoder` services

---
+++ app/services/jwt/encoder.rb
@@ -0,0 +1,13 @@
+module Jwt
+  class Encoder
+    SECRET_KEY = Rails.application.credentials.secret_key_base
+
+    def self.call(payload:)
+      JWT.encode(
+        payload,
+        SECRET_KEY,
+        "HS256"
+      )
+    end
+  end
+end
